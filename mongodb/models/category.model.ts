9819abc84ac0132a5d2f5946e01447b9234a4eb5
Daniel <daniel.liemng@gmail.com>
2023-12-19 18:56:03 -0500

    clerk webhook

--- mongodb/models/category.model.ts
+++ mongodb/models/category.model.ts
@@ -1,12 +1,12 @@
-import { Schema, model, models } from 'mongoose';
+import { Document, Schema, model, models } from 'mongoose';
+
+export interface ICategory extends Document {
+  _id: string;
+  name: string;
+}
 
 const CategorySchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  photo: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
 });
 
 const Category = models.Category || model('Category', CategorySchema);
