e0ff64696fc09b00b3b5798167dd625835f6dd2c
Daniel <daniel.liemng@gmail.com>
2023-12-21 12:16:07 -0500

    event details page

--- actions/event.action.ts
+++ actions/event.action.ts
@@ -2,9 +2,24 @@
 
 import { handleError } from '@/lib/helpers';
 import { connectDB } from '@/mongodb/database';
+import Category from '@/mongodb/models/category.model';
 import Event from '@/mongodb/models/event.model';
 import User from '@/mongodb/models/user.model';
 import { CreateEventParams } from '@/types';
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: 'organizer',
+      model: User,
+      select: '_id firstName lastName',
+    })
+    .populate({
+      path: 'category',
+      model: Category,
+      select: '_id name',
+    });
+};
 
 export const createEvent = async ({
   event,
@@ -29,6 +44,22 @@ export const createEvent = async ({
     console.log('NEWEVMT', newEvent);
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectDB();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error('Event not found');
+    }
+
+    return JSON.parse(JSON.stringify(event));
   } catch (error) {
     handleError(error);
   }
